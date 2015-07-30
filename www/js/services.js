feded81d5b52e7a95eb1687474c774c13b85a1da
Cesar Kamide <ckamide@gmail.com>
2015-07-30 15:22:57 -0300

    Sprint 3

--- www/js/services.js
+++ www/js/services.js
@@ -1,19 +1,21 @@
 angular.module('starter.services', ['restangular'])
 
 .factory('RestService', ['Restangular', function(Restangular) {
-    var apiKey = 'api_key=pubkey-0nsb9635xii6xfog5fq077gmahjle1z9'
+    var apiKey = 'pubkey-0nsb9635xii6xfog5fq077gmahjle1z9'
     var address = 'ckamide@gmail.com';
     var restAngular = Restangular.withConfig(function(Configurer) {
-      Configurer.setBaseUrl('https://api.mailgun.net/v3/address/validate');
+      Configurer.setBaseUrl('https://api.mailgun.net/v3/');
       Configurer.setDefaultRequestParams({ 
         apiKey: apiKey,
         address: address
       })
     });
-    restAngular.setResponseExtractor(function(response, operation) {
-        console.log(response.data);
+
+    var oneUser = restAngular.one('address');
+    oneUser.get().then(function(user) {
+      // GET /address/validate
+      console.log(user.getList('validate').$object);
     });
-    var messageService = restAngular.all('messages');
 
     return {
       setAdress: function(email) {
