2106f7fcdd3e88903091e1ee5e50dd4b588a354c
Cesar Kamide <ckamide@gmail.com>
2015-07-29 21:46:25 -0300

    Sprint 2
    consumo da api ainda não funciona

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,21 @@
 angular.module('starter.controllers', [])
 
-.controller('BuscaCtrl', function($scope) {})
+.controller('BuscaCtrl', function($scope, RestService) {
+	$scope.busca = {
+		email: 'teste'
+	}
+	$scope.buscar = function(){
+		RestService.setAdress($scope.busca.email);
+		console.log(RestService.getMessages());
+	}
+})
 
-.controller('ConsultasCtrl', function($scope, Chats) {
+.controller('ConsultasCtrl', function($scope, RestService) {
+	$scope.data = {
+		showDelete: false
+	};
 
+	$scope.onItemDelete = function(item) {
+    	$scope.items.splice($scope.items.indexOf(item), 1);
+  	};
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,26 @@
-angular.module('starter.services', [])
+angular.module('starter.services', ['restangular'])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+.factory('RestService', ['Restangular', function(Restangular) {
+    var apiKey = 'api_key=pubkey-0nsb9635xii6xfog5fq077gmahjle1z9'
+    var address = 'ckamide@gmail.com';
+    var restAngular = Restangular.withConfig(function(Configurer) {
+      Configurer.setBaseUrl('https://api.mailgun.net/v3/address/validate');
+      Configurer.setDefaultRequestParams({ 
+        apiKey: apiKey,
+        address: address
+      })
+    });
+    restAngular.setResponseExtractor(function(response, operation) {
+        console.log(response.data);
+    });
+    var messageService = restAngular.all('messages');
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
+    return {
+      setAdress: function(email) {
+        return address = email;
+      },
+      getMessages: function() {
+        return restAngular.getList();
       }
-      return null;
-    }
-  };
-});
+    };
+}]);
